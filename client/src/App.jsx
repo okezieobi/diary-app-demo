f47afb849e3893527134defaf09c504548863337
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-08-26 15:31:35 +0100

    Refractored home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
 import Home from './components/Home';
+import './App.css';
 
 function App() {
   return (
