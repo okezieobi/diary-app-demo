832d719ed1144d089bd413d0c96f4abd71511f71
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-08-27 02:00:33 +0100

    Added signin page for issue #17

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,21 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import Home from './components/Home';
 import Signup from './components/Signup';
-import './App.css';
+import Signin from './components/Signin';
 
 function AppTest() {
   return (
-      <Switch>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+    <Switch>
+      <Route path="/signin">
+        <Signin />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
   );
 }
 
@@ -23,7 +26,7 @@ function App() {
     <BrowserRouter>
       <AppTest />
     </BrowserRouter>
-  )
+  );
 }
 
 export { App, AppTest };
