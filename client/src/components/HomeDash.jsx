0f279d4fb59770ba44a9d79f0e95258e6318bfd5
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-08-29 01:20:37 +0100

    Added tables to dashboard, fixes #2

--- client/src/components/HomeDash.jsx
+++ client/src/components/HomeDash.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import MUIDataTable from 'mui-datatables';
 
 import Dashboard from './Dashboard';
+
+const columns = ['title', 'body', 'created on', 'updated on'];
+const options = {
+  filterType: 'checkbox',
+};
 
 export default function HomeDash() {
   return (
     <Dashboard>
-      <Typography paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-        facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-        gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-        donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-        adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-        Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-        imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-        arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-        donec massa sapien faucibus et molestie ac.
-      </Typography>
+      <MUIDataTable
+        title="Entries"
+        columns={columns}
+        options={options}
+      />
     </Dashboard>
   );
 }
