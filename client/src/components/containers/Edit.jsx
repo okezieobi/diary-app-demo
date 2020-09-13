76d3cafaf0fc4bdb1bd9022f6adeca5e3aedc711
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-09-13 22:57:40 +0100

    Fixes #5 Users can view and enable reminder settings.

--- client/src/components/containers/Edit.jsx
+++ client/src/components/containers/Edit.jsx
@@ -4,10 +4,10 @@ import Dashboard from '../layouts/Dashboard';
 import Form from '../layouts/Form';
 
 export default function Compose() {
-  const [title, body] = JSON.parse(localStorage.getItem('clickedRow'));
+  const rowData = JSON.parse(localStorage.getItem('clickedRow'));
   return (
     <Dashboard homeSelect>
-      <Form title={title} body={body} />
+      <Form title={rowData ? rowData[0] : ''} body={rowData ? rowData[1] : ''} />
     </Dashboard>
   );
 }
